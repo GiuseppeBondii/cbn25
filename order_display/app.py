76f5a84385c17903f68a242527bbff90e2d20c52
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2025-04-28 12:48:36 +0200

    Add Order Display

---
+++ order_display/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+orders = []
+pwd = 'SucaZizza'
+authToken = 0
+
+@app.route('/')
+def index():
+    return render_template('display.html', orders=orders)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if authToken == 0:
+        return redirect('/login')
+    
+    if request.method == 'POST':
+        order_number = request.form.get('order_number')
+        if 'add' in request.form and order_number:
+            orders.append(order_number)
+        elif 'remove' in request.form and order_number:
+            try:
+                orders.remove(order_number)
+            except ValueError:
+                pass
+    return render_template('admin.html', orders=orders)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global authToken
+    if authToken == 1:
+        return redirect('/admin')
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == pwd:
+            authToken = 1
+            return redirect('/admin')
+        else:
+            return "Password incorrect"
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
